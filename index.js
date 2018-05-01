31be848452a53336b004030706c1ca9c863ec269
treml <ftreml@gmail.com>
2018-05-01 23:48:09 +0200

    Styling

--- index.js
+++ index.js
@@ -2,38 +2,38 @@ const debug = require('debug')('botium-connector-echo')
 
 class BotiumConnectorEcho {
   constructor ({ queueBotSays }) {
-   this.queueBotSays = queueBotSays
+    this.queueBotSays = queueBotSays
   }
 
   Validate () {
-   debug('Validate called')
-   return Promise.resolve()
+    debug('Validate called')
+    return Promise.resolve()
   }
 
   Build () {
-   debug('Build called')
-   return Promise.resolve()
+    debug('Build called')
+    return Promise.resolve()
   }
 
   Start () {
-   debug('Start called')
-   return Promise.resolve()
+    debug('Start called')
+    return Promise.resolve()
   }
 
   UserSays (msg) {
-   debug('UserSays called, echo back')
-   const botMsg = { sender: 'bot', sourceData: msg.sourceData, messageText: msg.messageText }
-   this.queueBotSays(msg)
+    debug('UserSays called, echo back')
+    const botMsg = { sender: 'bot', sourceData: msg.sourceData, messageText: msg.messageText }
+    this.queueBotSays(botMsg)
   }
 
   Stop () {
-   debug('Stop called')
-   return Promise.resolve()
+    debug('Stop called')
+    return Promise.resolve()
   }
 
   Clean () {
-   debug('Clean called')
-   return Promise.resolve()
+    debug('Clean called')
+    return Promise.resolve()
   }
 }
 
