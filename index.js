c780e51909ae889890564e57441f864be6339d47
ftreml <ftreml@gmail.com>
2018-11-09 12:56:53 +0000

    additional UI content

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 const debug = require('debug')('botium-connector-echo')
 
+const answers = [
+  {
+    input: ['buttons', 'show me buttons', 'show me some buttons', 'give me buttons'],
+    output: {
+      messageText: 'Here are some buttons',
+      buttons: [
+        { text: 'First Button' },
+        { text: 'Second Button' }
+      ]
+    }
+  },
+  {
+    input: ['picture', 'show me a picture', 'give me a picture'],
+    output: {
+      messageText: 'Here is a picture',
+      media: [
+        { altText: 'Botium Logo', mediaUri: 'http://www.botium.at/img/logo.png' }
+      ]
+    }
+  },
+  {
+    input: ['card', 'show me a card', 'give me a card'],
+    output: {
+      messageText: 'Here is a card',
+      cards: [
+        {
+          text: 'Botium is great!',
+          image: { mediaUri: 'http://www.botium.at/img/logo.png' },
+          buttons: [
+            { text: 'First Button' },
+            { text: 'Second Button' }
+          ]
+        }
+      ]
+    }
+  }
+]
+
 class BotiumConnectorEcho {
   constructor ({ queueBotSays }) {
     this.queueBotSays = queueBotSays
@@ -22,8 +60,12 @@ class BotiumConnectorEcho {
 
   UserSays (msg) {
     debug('UserSays called, echo back')
-    const botMsg = { sender: 'bot', sourceData: msg.sourceData, messageText: msg.messageText }
-    this.queueBotSays(botMsg)
+    const template = answers.find((a) => a.input.indexOf(msg.messageText) >= 0)
+    if (template) {
+      setTimeout(() => this.queueBotSays(Object.assign({}, { sender: 'bot', sourceData: msg }, template.output)), 0)
+    } else {
+      setTimeout(() => this.queueBotSays({ sender: 'bot', sourceData: msg, messageText: 'You said: ' + msg.messageText }), 0)
+    }
   }
 
   Stop () {
